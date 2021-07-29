c6ae1f902e60bfa723e36f34c2253ae3480654e6
fioralda <fioraldaosmenai99@gmail.com>
2021-07-29 13:06:06 +0300

    adding styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import { Image, Flex, Text, Box, Spacer, Link, Button } from '@chakra-ui/react';
-import { List, ListItem } from '@chakra-ui/react';
+import { List, ListItem, Stack } from '@chakra-ui/react';
 
 import Logo from '../assets/logo.png';
 import AppStore from '../assets/app-store.png';
 import GooglePlay from '../assets/google-play.png';
 
+import footer_facebook from '../assets/footer_facebook.svg';
+import footer_instagram from '../assets/footer_instagram.svg';
+import footer_linkedin from '../assets/footer_linkedin.svg';
+import footer_twitter from '../assets/footer_twitter.svg';
+
 const Footer = () => {
   return (
     <Box>
-      <Flex bgColor='gray.100' direction='column' pl='400' pt='10'>
-        <Image src={Logo} alt='Logo' w='20' h='25' />
+      <Flex bgColor='gray.100' direction='column' pl='400' pt='10' pb='20'>
+        <Image src={Logo} alt='Logo' w='20' h='30' />
         <Spacer />
         <Flex>
           <Flex fontWeight='bold' fontSize='md'>
@@ -17,30 +22,35 @@ const Footer = () => {
           </Flex>
           {/* <Spacer /> */}
           <Flex pl='60'>
-            <List spacing='2' fontSize='sm'>
+            <List fontSize='sm'>
               <Text fontWeight='bold' pb='2' fontSize='md'>
-                Title 1
+                Let's do this together
               </Text>
-              <ListItem>Lorem ipsum dolor sit amet</ListItem>
-              <ListItem>Consectetur adipiscing elit</ListItem>
-              <ListItem>Integer molestie lorem at massa</ListItem>
-              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+              <ListItem>For couriers</ListItem>
+              <ListItem>For restaurants</ListItem>
+              <ListItem>For stores</ListItem>
+              <ListItem>For companies</ListItem>
+              <ListItem>For Jobs</ListItem>
             </List>
           </Flex>
           <Flex pl='60'>
-            <List spacing='2' fontSize='sm'>
+            <List fontSize='sm'>
               <Text fontWeight='bold' pb='2' fontSize='md'>
-                Title 2
+                iWolt links
               </Text>
-              <ListItem>Lorem ipsum dolor sit amet</ListItem>
-              <ListItem>Consectetur adipiscing elit</ListItem>
-              <ListItem>Integer molestie lorem at massa</ListItem>
-              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+              <ListItem>Contact</ListItem>
+              <ListItem>Media</ListItem>
+              <ListItem>Support</ListItem>
+              <ListItem>iWolt blog</ListItem>
+              <ListItem>Engineering blog</ListItem>
+              <ListItem>Responsibility</ListItem>
+              <ListItem>Security</ListItem>
+              <ListItem>About us</ListItem>
             </List>
           </Flex>
         </Flex>
         <Flex direction='column'>
-          <Flex p='2'>
+          <Flex p='2' pb='15'>
             <Link
               href='https://apps.apple.com/fi/app/wolt/id943905271'
               isExternal
@@ -69,6 +79,19 @@ const Footer = () => {
               </Button>
             </Link>
           </Flex>
+        </Flex>
+        <Flex p='10'>
+          <p>&copy; iWolt 2021</p>
+          <Text pr='25' pl='237' pr='8'>
+            Accessibility statement
+          </Text>
+          <Text pr='20'>Terms and conditions</Text>
+          <Stack direction='row' spacing='6' pb='2' pl='-1.5'>
+            <Image src={footer_facebook} w='7' h='7' />
+            <Image src={footer_instagram} w='7' h='7' />
+            <Image src={footer_linkedin} w='7' h='7' />
+            <Image src={footer_twitter} w='7' h='7' />
+          </Stack>
         </Flex>
       </Flex>
     </Box>
